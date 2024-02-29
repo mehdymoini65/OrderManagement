16b63b24f9fa829515de3e5f4902c2fb76904f8b
Mehdy <mehdy.moini@yahoo.com>
2024-02-29 15:21:45 +0330

    Add interface of repository to domain

---
+++ src/OrderManagement.Domain/Orders/Repositories/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using OrderManagement.Orders.Entities;
+using Volo.Abp.Domain.Repositories;
+
+namespace OrderManagement.Orders.Repositories;
+
+public interface IOrderRepository : IRepository<Order, Guid>
+{
+
+}
---
+++ src/OrderManagement.Domain/Products/Repositories/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using OrderManagement.Products.Entities;
+using Volo.Abp.Domain.Repositories;
+
+namespace OrderManagement.Products.Repositories;
+
+public interface IProductRepository : IRepository<Product, Guid>
+{
+
+}
