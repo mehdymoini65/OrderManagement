470dce7b91de1b56cf82203cbb10ee48d922ae7a
Mehdy <mehdy.moini@yahoo.com>
2024-02-29 14:35:24 +0330

    Add product to domain

---
+++ src/OrderManagement.Domain/Products/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Volo.Abp.Domain.Entities;
+
+namespace OrderManagement.Products.Entities;
+
+public class Product : AggregateRoot<Guid>
+{
+    public Product(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; private set; }
+
+}
